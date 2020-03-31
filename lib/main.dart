f32f99969ec8410f3d3d308340bca288ae972409
Sean Ervinson <ervinsonong@gmail.com>
2020-03-31 18:03:39 +0800

    Implemented asset bloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:color_picker/components/home/bloc/asset_bloc.dart';
 import 'package:color_picker/screens/home_screen.dart';
 import 'package:color_picker/values/values.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(ColorPickerApp());
@@ -15,7 +17,10 @@ class ColorPickerApp extends StatelessWidget {
       routes: {
         "home": (context) => HomeScreen(),
       },
-      home: HomeScreen(),
+      home: BlocProvider(
+        create: (context) => AssetBloc()..add(LoadImages()),
+        child: HomeScreen(),
+      ),
     );
   }
 }
