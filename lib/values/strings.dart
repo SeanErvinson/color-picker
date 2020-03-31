2bbc0621bf2f65bd4e6b9a3d7675bd2a5010de4f
Sean Ervinson <ervinsonong@gmail.com>
2020-03-31 22:35:01 +0800

    Added const value for appraise screen

--- lib/values/strings.dart
+++ lib/values/strings.dart
@@ -2,4 +2,9 @@ class Strings {
   static const String appName = "Color Picker";
   static const String gallery = "Gallery";
   static const String emptyGallery = "No images available.";
+  static const String hsvColorLabel = "HSV";
+  static const String cmykColorLabel = "CMYK";
+  static const String rgbColorLabel = "RGB";
+  static const String hexColorLabel = "HEX";
+  static const String copyClipboard = "Copy to clipboard";
 }
