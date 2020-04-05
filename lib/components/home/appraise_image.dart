44f2893909476b14bb890940604b399b8f60d000
Sean Ervinson <ervinsonong@gmail.com>
2020-04-05 21:54:53 +0800

    Removed unused code

--- lib/components/home/appraise_image.dart
+++ lib/components/home/appraise_image.dart
@@ -5,16 +5,6 @@ import 'package:color_picker/models/coordinate.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-// class AppraiseImage extends StatefulWidget {
-//   final File _imageFile;
-
-//   const AppraiseImage({Key key, File imageFile})
-//       : this._imageFile = imageFile,
-//         super(key: key);
-//   @override
-//   _AppraiseImageState createState() => _AppraiseImageState(_imageFile);
-// }
-
 class AppraiseImage extends StatelessWidget {
   final File _imageFile;
   const AppraiseImage({Key key, File imageFile}) : this._imageFile = imageFile;
