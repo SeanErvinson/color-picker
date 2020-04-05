8db3dcc05f4418f64e05f0cd88c092638e63c0b8
Sean Ervinson <ervinsonong@gmail.com>
2020-04-05 17:51:33 +0800

    Locked to portrait mode
    Added appraise bloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
+import 'package:color_picker/components/home/bloc/appraise_bloc.dart';
 import 'package:color_picker/components/home/bloc/asset_bloc.dart';
 import 'package:color_picker/screens/screens.dart';
 import 'package:color_picker/values/values.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
@@ -11,14 +13,22 @@ void main() {
 class ColorPickerApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: Strings.appName,
       routes: {
         "home": (context) => HomeScreen(),
       },
-      home: BlocProvider(
-        create: (context) => AssetBloc()..add(LoadImages()),
+      home: MultiBlocProvider(
+        providers: [
+          BlocProvider<AssetBloc>(
+            create: (context) => AssetBloc(),
+          ),
+          BlocProvider<AppraiseBloc>(
+            create: (context) => AppraiseBloc(),
+          ),
+        ],
         child: HomeScreen(),
       ),
     );
