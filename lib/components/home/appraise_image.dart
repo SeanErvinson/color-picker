9a67699d6832f1f1abf5d20b86720ac827eac6f2
Sean Ervinson <ervinsonong@gmail.com>
2020-04-05 21:46:36 +0800

    Replaced stateful with statelss

--- lib/components/home/appraise_image.dart
+++ lib/components/home/appraise_image.dart
@@ -1,23 +1,23 @@
 import 'dart:io';
 
 import 'package:color_picker/components/home/bloc/appraise_bloc.dart';
-import 'package:color_picker/models/point.dart';
+import 'package:color_picker/models/coordinate.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class AppraiseImage extends StatefulWidget {
+// class AppraiseImage extends StatefulWidget {
+//   final File _imageFile;
+
+//   const AppraiseImage({Key key, File imageFile})
+//       : this._imageFile = imageFile,
+//         super(key: key);
+//   @override
+//   _AppraiseImageState createState() => _AppraiseImageState(_imageFile);
+// }
+
+class AppraiseImage extends StatelessWidget {
   final File _imageFile;
-
-  const AppraiseImage({Key key, File imageFile})
-      : this._imageFile = imageFile,
-        super(key: key);
-  @override
-  _AppraiseImageState createState() => _AppraiseImageState(_imageFile);
-}
-
-class _AppraiseImageState extends State<AppraiseImage> {
-  final File imageFile;
-  _AppraiseImageState(this.imageFile);
+  const AppraiseImage({Key key, File imageFile}) : this._imageFile = imageFile;
 
   @override
   Widget build(BuildContext context) {
@@ -26,18 +26,18 @@ class _AppraiseImageState extends State<AppraiseImage> {
         BlocProvider.of<AppraiseBloc>(context).add(CancelAppraise());
       },
       onPanDown: (DragDownDetails details) {
-        Point point = Point(
+        Coordinate point = Coordinate(
             x: details.globalPosition.dx.toInt(),
             y: details.globalPosition.dy.toInt());
-        BlocProvider.of<AppraiseBloc>(context).add(UpdateAppraise());
+        BlocProvider.of<AppraiseBloc>(context).add(UpdateAppraise(point));
       },
       onPanUpdate: (DragUpdateDetails details) {
-        Point point = Point(
+        Coordinate point = Coordinate(
             x: details.globalPosition.dx.toInt(),
             y: details.globalPosition.dy.toInt());
-        BlocProvider.of<AppraiseBloc>(context).add(UpdateAppraise());
+        BlocProvider.of<AppraiseBloc>(context).add(UpdateAppraise(point));
       },
-      child: Image.file(imageFile),
+      child: Image.file(_imageFile),
     );
   }
 }
